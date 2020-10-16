2ad6c4754b4b5b1b9f7e57d65247232df38856b2
Ron Northcutt <rlnorthcutt@gmail.com>
2020-10-16 10:02:06 -0500

    adding theme

---
+++ df_custom_components/components/example_vue_unicorn/example_vue_unicorn.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#example_vue_unicorn',
+  data: {
+    name: 'A Beautiful Vuenicorn',
+    mainColor: '#662d91',
+    tailAccentColor: '#36c1d9',
+    bodyColor: '#efe2B4',
+    footColor: '#05f1CD',
+    wingColor: '#fff',
+    hornColor: '#c69'
+  }
+})
